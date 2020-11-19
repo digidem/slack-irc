504f0782e6e2b943e507c29ddbba5a04bf01a74e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-11-19 12:36:52 +0000

    feat: Add commands on connect
    Used to authenticate ddem-bot user

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const config = {
   server: process.env.IRC_SERVER,
   token: process.env.SLACK_TOKEN,
   avatarUrl: 'https://robohash.org/$username.png',
-  channelMapping: JSON.parse(process.env.CHANNEL_MAPPING)
+  channelMapping: JSON.parse(process.env.CHANNEL_MAPPING),
+  autoSendCommands: JSON.parse(process.env.AUTO_SEND_COMMANDS)
 }
 
 slackIRC(config)
